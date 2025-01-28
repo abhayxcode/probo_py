dbb8a04ad35c3eac1f08af06fe5a0d9be21a6a5d
developer-abhay <abhaysharma.developer@gmail.com>
2025-01-28 08:13:09 +0530

    feat: auth utils

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -1,18 +1,43 @@
-from typing import Optional
-from datetime import timedelta
+import os
+from dotenv import load_dotenv
+import bcrypt
+import jwt
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Hash a password
 def hash_password(password: str) -> str:
-    return password
+    salt = bcrypt.gensalt()
+    bytes = password.encode('utf-8')
+    hash = bcrypt.hashpw(bytes,salt)  
+    hased_password = hash.decode('utf-8') # hashed password
+    return hased_password
 
 # Verify a hashed password
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return True
+    plain_password_bytes = plain_password.encode('utf-8')
+    is_match = bcrypt.checkpw(plain_password_bytes,hashed_password.encode('utf-8'))
+    return is_match
 
 # Create JWT token
 def create_access_token(data: dict) -> str:
-    return 'token'
+    # Access JWT_SECRET
+    JWT_SECRET = os.getenv("JWT_SECRET")
+
+    if not JWT_SECRET:
+        raise ValueError("JWT_SECRET environment variable is not set")
+    
+    encoded_jwt = jwt.encode(data , JWT_SECRET, algorithm='HS256')
+    return encoded_jwt
 
 # Decode JWT token
 def verify_token(token: str):
-    return 'payload'
+    # Access JWT_SECRET
+    JWT_SECRET = os.getenv("JWT_SECRET")
+
+    if not JWT_SECRET:
+        raise ValueError("JWT_SECRET environment variable is not set")
+    
+    payload = jwt.decode(token, JWT_SECRET , algorithms=['HS256'])
+    return payload
